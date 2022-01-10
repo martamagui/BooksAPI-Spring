9737430cbcaac086b6cb5112e678df478f3136c4
martamagui <marta.molina@live.u-tad.com>
2022-01-10 11:46:19 +0100

    scriptUpdate

---
+++ MySql-Scripts/books_inserts.sql
@@ -0,0 +1,69 @@
+INSERT INTO
+  bookstable(titulo, autor, categoria, precio, imgUrl)
+VALUES(
+    '50 palos: ... y sigo soñando (No Ficción)',
+    'Pau Donés',
+    'Biografía',
+    '17.95',
+    'https://images-na.ssl-images-amazon.com/images/I/518ladyzBhL._SX324_BO1,204,203,200_.jpg'
+  ),(
+    'C/C++. Curso de programación (Manuales Imprescindibles)',
+    'Miguel Angel Acera',
+    'Programacion',
+    '27.31',
+    'https://images-na.ssl-images-amazon.com/images/I/41SmMXeXYCL._SX258_BO1,204,203,200_.jpg'
+  ),(
+    'Clean JavaScript: Aprende a aplicar Código Limpio, SOLID y Testing',
+    'Miguel A. Gómez ',
+    'Programacion',
+    '19.97 €',
+    'https://images-na.ssl-images-amazon.com/images/I/51q4UQsslIL.jpg'
+  ),(
+    'Elon Musk: El empresario que anticipa el futuro',
+    'Ashlee Vance',
+    'Biografía',
+    '17.00',
+    'https://m.media-amazon.com/images/I/51qeHcSeWyS.jpg'
+  ),(
+    'El Libro Negro del Programador',
+    'Rafael Gómez Blanes',
+    'Programacion',
+    '15.77',
+    'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1398892720l/22027034.jpg'
+  ),(
+    'Frida Kahlo. Una biografía (Lumen Gráfica)',
+    ' María Hesse ',
+    'Biografía',
+    '20.80',
+    'https://images-na.ssl-images-amazon.com/images/I/514M5-XnvOL._SX344_BO1,204,203,200_.jpg'
+  ),(
+    'La práctica del color',
+    'Lisa Solomón',
+    'Arte',
+    '23.65',
+    'https://imagessl2.casadellibro.com/a/l/t7/02/9788425233302.jpg'
+  ),(
+    'Marvel By Design: Graphic Design Strategies of the Worlds Greatest Comic Company',
+    ' Liz Gestalten',
+    'Arte',
+    '48.35',
+    'https://images-na.ssl-images-amazon.com/images/I/61ms8dkkWVL.jpg'
+  ),(
+    'Por si las voces vuelven (No Ficción)',
+    'Ángel Martín',
+    'Biografía',
+    '17.00',
+    'https://images-na.ssl-images-amazon.com/images/I/8137pZXGy+L.jpg'
+  ),(
+    'Programando Videojuegos: JavaScript',
+    'Iván Fasheh',
+    'Programacion',
+    '19.45',
+    'https://images-na.ssl-images-amazon.com/images/I/315115dvA4L._SX367_BO1,204,203,200_.jpg'
+  ),(
+    'The Artists Way: A Spiritual Path to Higher Creativity',
+    'Julia Cameron',
+    'Arte',
+    '18.95',
+    'https://images-na.ssl-images-amazon.com/images/I/81tjEWZ8t7L.jpg'
+  );
